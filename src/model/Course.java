26e2c8f322cb7ff2adf9afecde8b0e8815a92c73
Lovisa Jansson <lo6788ja.s@student.lu.se>
2018-12-12 17:39:51 +0100

    Update Course.java

---
+++ src/model/Course.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Course {
+
+	
+	private String courseCode;
+	private String name;
+	private double credits;
+	
+	//getters & setters
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getCredits() {
+		return credits;
+	}
+	public void setCredits(double credits) {
+		this.credits = credits;
+	}
+	
+	
+}
