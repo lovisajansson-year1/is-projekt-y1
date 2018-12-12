2da0fc7b238e9fcde76e7993be5c17765aae2259
Marc Hulterström <ma6815hu-s@student.lu.se>
2018-12-12 07:14:11 +0100

    Funkar commit
    Testar att det är uppsatt riktigt

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,8 +3,9 @@ package Test;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub	
+		
+		System.out.println("Funka");
 		System.out.println("hej");
 		System.out.println("då");
 	}
