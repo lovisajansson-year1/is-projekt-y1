0563086d411075c382265f711c21f8f24a2afccc
Lovisa Jansson <lo6788ja.s@student.lu.se>
2018-12-13 17:53:24 +0100

    update course

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,18 +1,35 @@
 package model;
-
+import java.util.ArrayList;
 public class Course {
 
 	
-	private String courseCode;
+	private String courseCode; // unique, begins with capital C followed by number between 1000 and 9999
 	private String name;
 	private double credits;
+	private ArrayList<WrittenExam> writtenExams = new ArrayList<WrittenExam> ();
 	
 	//getters & setters
 	public String getCourseCode() {
 		return courseCode;
 	}
+	public ArrayList<WrittenExam> getWrittenExams() {
+		return writtenExams;
+	}
+	public void setWrittenExams(ArrayList<WrittenExam> writtenExams) {
+		this.writtenExams = writtenExams;
+	}
 	public void setCourseCode(String courseCode) {
-		this.courseCode = courseCode;
+		
+		if(courseCode.charAt(0)==('C')) {
+			String courseCodeSubstring = courseCode.substring(1,5);
+			try {
+				int code = Integer.parseInt("courseCodeSubstring");
+			} catch(Exception notnumbers) {
+				System.out.println("Coursecode must be a C followed by 5 numbers");
+			}
+			this.courseCode = courseCode;
+		}
+			
 	}
 	public String getName() {
 		return name;
