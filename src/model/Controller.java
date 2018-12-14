4ea364512094a992a537b0d78dc0b1dc5ded7d15
lovisajansson <44163582+lovisajansson@users.noreply.github.com>
2018-12-14 16:33:50 +0100

    Start på controller

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,5 +1,18 @@
 package model;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class Controller {
+	
+	@FXML
+	private
+	
+	
 	
 }
