77034b47af193517bb148978899e76d1a9ca99ac
Lovisa Jansson <lo6788ja.s@student.lu.se>
2018-12-14 15:02:12 +0100

    update

--- src/model/Course.java
+++ src/model/Course.java
@@ -6,30 +6,48 @@ public class Course {
 	private String courseCode; // unique, begins with capital C followed by number between 1000 and 9999
 	private String name;
 	private double credits;
-//	private ArrayList<WrittenExam> writtenExams = new ArrayList<WrittenExam> ();
+	private ArrayList<WrittenExam> writtenExams = new ArrayList<WrittenExam> ();
+	private CourseRegister courseRegister;
 	
 	//getters & setters
 	public String getCourseCode() {
 		return courseCode;
 	}
-//	public ArrayList<WrittenExam> getWrittenExams() {
-//		return writtenExams;
-//	}
-//	public void setWrittenExams(ArrayList<WrittenExam> writtenExams) {
-//		this.writtenExams = writtenExams;
-//	}
-	public void setCourseCode(String courseCode) {
-		
-		if(courseCode.charAt(0)==('C')) {
-			String courseCodeSubstring = courseCode.substring(1,5);
-			try {
-				int code = Integer.parseInt("courseCodeSubstring");
-			} catch(Exception notnumbers) {
-				System.out.println("Coursecode must be a C followed by 5 numbers");
+	public ArrayList<WrittenExam> getWrittenExams() {
+		return writtenExams;
+	}
+	public void setWrittenExams(ArrayList<WrittenExam> writtenExams) {
+		this.writtenExams = writtenExams;
+	}
+	// genererar kursid och checkar så att det inte redan finns
+	public String generateCourseCode(CourseRegister courseRegister) {
+		ArrayList<Course> course = courseRegister.getCourseRegister();
+		String newCode;
+		for(int i=10000+course.size(); i<100000; i++) {
+			newCode = "C" + Integer.toString(i);
+			for(Course tmp: course) {
+				if(tmp.getCourseCode().equals(newCode)) {
+					newCode = null;
+					break;
+				}
 			}
-			this.courseCode = courseCode;
+			if(newCode != null) {
+				return this.courseCode = newCode;
+			}
 		}
-			
+		return null;
+	}
+
+	
+	
+	public CourseRegister getCourseRegister() {
+		return courseRegister;
+	}
+	public void setCourseRegister(CourseRegister courseRegister) {
+		this.courseRegister = courseRegister;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
 	}
 	public String getName() {
 		return name;
