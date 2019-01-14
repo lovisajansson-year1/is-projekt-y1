3ccd49e2e2dbf2e9ef55fcdd27a38d765b20a1b4
Erik Larsson <er8730la-s@student.lu.se>
2019-01-14 11:29:28 +0100

    Fix

--- src/model/WrittenExam.java
+++ src/model/WrittenExam.java
@@ -18,14 +18,7 @@ public class WrittenExam {
     private ArrayList<Result> results = new ArrayList<Result>();
     private Course course;
 
-
-    /**
-     * Create exam for a location connected to course.
-     *
-     * @param String location
-     * @param Course course
-     * @return WrittenExam
-     */
+    
     public WrittenExam (String location, Course course) {
         this.examID = this.generateExamId(course);
         this.location = location;
@@ -33,74 +26,11 @@ public class WrittenExam {
         course.addWrittenExam(this);
     }
 
-    /**
-     * Calculates average of results for given exam.
-     *
-     * @return double average
-     */
-    public double getAverage() {
-    	double average = 0;
-    	if(results.size() > 0) {
-	    	for (Result res : results) {
-	    		average += res.getPoints();
-	    	}
-	    	average = average/getResults().size();
-    	}
-        return average;
+	public String getExamID() {
+        return examID;
     }
 
-    /**
-     * Calculates median of results for given exam.
-     *
-     * @return double median
-     */
-    public double getMedian() {
-    	double med = 0;
-    	ArrayList<Double> sortedResults = new ArrayList<Double>();
-    	for(Result x : results) sortedResults.add((double) x.getPoints());
-    	Collections.sort(sortedResults);
-    	if(sortedResults.size() > 0) {
-    		if(sortedResults.size() % 2 == 0) {
-    			double med1 = sortedResults.get((int)(results.size()/2));
-    			double med2 = sortedResults.get((int)(results.size()/2)-1);
-    			median = (med1+med2) / 2;
-    		} else {
-    			median = sortedResults.get((int)((results.size()/2)+0.5));
-    		}
-    	}
-		return median;
-    }
-
-    /**
-     * The number of students who passed a given exam.
-     *
-     * @return int count
-     */
-    public int passed() {
-    	int count = 0;
-    	if(results.size() > 0) {
-    		for(Result x : results) {
-    			if(x.getPoints() >= 50) {
-    				count += 1;
-    			}
-    		}
-    	}
-    	return count;
-    }
-
-    /**
-     * The percentage of the students who passed the exam.
-     *
-     * @return double percentage
-     */
-    public double passPercentage() {
-    	if (results.size() > 0) {
-    		return 100 *( ((double)passed()) / results.size()) ;
-    	} else {
-    		return 0;
-    	}
-    }
-    public String generateExamId(Course course) {
+	public String generateExamId(Course course) {
 		ArrayList<Course> allCourses = course.getCourseRegister().getCourses();
 		ArrayList<WrittenExam> exams = course.getWrittenExams();
 		String newId;
@@ -121,8 +51,107 @@ public class WrittenExam {
 		return null;
 	}
 
+    public String getLocation() {
+        return location;
+    }
 
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
+    public int getMaxPoints() {
+        return MAX_VALUE;
+    }
+
+   
+
+	public ArrayList<Result> getResults() {
+		return results;
+	}
+
+	public void setResults(ArrayList<Result> results) {
+		this.results = results;
+	}
+
+    public void addResult(Result result) {
+    	this.results.add(result);
+    }
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	
+    /**
+     * Calculates average of results for given exam.
+     *
+     * @return double average
+     */
+    public double getAverage() {
+    	double average = 0;
+    	if(results.size() > 0) {
+	    	for (Result res : results) {
+	    		average += res.getPoints();
+	    	}
+	    	average = average/getResults().size();
+    	}
+        return average;
+    }
+    
+    /**
+     * Calculates median of results for given exam.
+     *
+     * @return double median
+     */
+    public double getMedian() {
+    	double median = 0;
+    	ArrayList<Double> sortedResults = new ArrayList();
+    	for(Result x : results) sortedResults.add((double) x.getPoints());
+    	Collections.sort(sortedResults);
+    	if(sortedResults.size() > 0) {
+    		if(sortedResults.size() % 2 == 0) {
+    			double med1 = sortedResults.get((int)(results.size()/2));
+    			double med2 = sortedResults.get((int)(results.size()/2)-1);
+    			median = (med1+med2) / 2;
+    		} else {
+    			median = sortedResults.get((int)((results.size()/2)+0.5));
+    		}
+    	}
+		return median;
+    }
+    
+    /**
+     * The number of students who passed a given exam.
+     *
+     * @return int count
+     */
+    public int passed() {
+    	int count = 0;
+    	if(results.size() > 0) {
+    		for(Result x : results) {
+    			if(x.getPoints() >= 50) {
+    				count += 1;
+    			}
+    		}
+    	}
+    	return count;
+    }
+    
+    /**
+     * The percentage of the students who passed the exam.
+     *
+     * @return double av
+     */
+    public double passPercentage() {
+    	if (results.size() > 0) {
+    		return 100 *( ((double)passed()) / results.size()) ;
+    	} else {
+    		return 0;
+    	}
+    }
 
 
 }
